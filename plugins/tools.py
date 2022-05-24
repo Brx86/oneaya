b38bf84846a1b67e6e922e7ec4c2e9ad02143ccf
Brx86 <brx86@qq.com>
2022-05-24 20:37:17 +0800

    add asyncio.create_subprocess_exec

--- plugins/tools.py
+++ plugins/tools.py
@@ -2,20 +2,28 @@ import asyncio
 from nonebot.log import logger
 
 
-async def aiorun(cmd):
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
+async def aiorun(cmd, shell=True):
+    proc = (
+        await asyncio.create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        if shell
+        else await asyncio.create_subprocess_exec(
+            *cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
     )
     logger.warning(f"Running command: {cmd}")
     stdout, stderr = await proc.communicate()
-    if proc.returncode:
-        logger.error(f"[{cmd!r} exited with {proc.returncode}]")
     if stdout:
         return stdout.decode()
-    if stderr:
-        logger.warning(f"[stderr]\n{stderr.decode()}")
+    if proc.returncode:
+        logger.error(f"[{cmd!r} exited with {proc.returncode}]")
+        if stderr:
+            logger.warning(f"[stderr]\n{stderr.decode()}")
 
 
 async def silicon(text, lang="bash", rp=False):
@@ -26,6 +34,6 @@ async def silicon(text, lang="bash", rp=False):
         textlist = text.splitlines()
         text = text if len(textlist) < 100 else "\n".join(textlist[:100])
         await f.write(text + "......")
-    cmd = f"silicon /tmp/text -o /tmp/text.png -l {lang} -f LXGWWenKaiMono -b #000000 --no-window-controls"
+    cmd = f"silicon /tmp/text -o/tmp/text.png -l{lang} -fLXGWWenKaiMono -b#000000 --no-window-controls"
     await aiorun(cmd)
     return "file:///tmp/text.png"
